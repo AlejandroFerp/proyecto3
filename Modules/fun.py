a7c1bfe7e74c60ab1e5330fb96c66595e1954c34
AlejandroFerp <122363677+AlejandroFerp@users.noreply.github.com>
2023-09-12 00:51:11 +0200

    Add files via upload

---
+++ Modules/fun.py
@@ -0,0 +1,20 @@
+import pandas as pd
+# import os
+import pickle
+import base64
+import sklearn
+
+PAGE_CONFIG = {"page_title"             : "Precio Cohces Segunda Mano",
+                "page_icon"             : ":car:",
+                "layout"                : "wide",
+                "initial_sidebar_state" : "expanded"}
+
+csv_files = ['sources\stream.csv']
+
+def preprocesamiento_ml(fila):
+    
+    
+    
+    
+    
+    return datos
